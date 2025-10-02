5d658d72c5e32e2c08c892f70864fe13e1df715b
rylander925 <rylander925@gmail.com>
2025-10-01 21:59:51 -0700

    Added ReviewNode struct

--- main.cpp
+++ main.cpp
@@ -2,3 +2,23 @@
 COMSC-210 | Lab 18 | Rylan Der
 IDE Used: Visual Studio Code
  */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+ using namespace std;
+
+ struct ReviewNode {
+    string comment;
+    double rating;
+    ReviewNode* next;
+
+    ReviewNode() { comment = "No comment"; rating = -1; next = nullptr; }
+    ReviewNode(string c, double r): comment(c), rating(r), next(nullptr) { }
+    ReviewNode(string c, double r, ReviewNode* next): comment(c), rating(r), next(next) { }
+ }
+
+ int main() {
+ }
+
