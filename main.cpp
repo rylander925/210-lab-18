669d0daa610d32efb504d92f2e04b2e4297086f5
rylander925 <rylander925@gmail.com>
2025-10-01 22:05:35 -0700

    Added function stubs

--- main.cpp
+++ main.cpp
@@ -17,8 +17,32 @@ IDE Used: Visual Studio Code
     ReviewNode() { comment = "No comment"; rating = -1; next = nullptr; }
     ReviewNode(string c, double r): comment(c), rating(r), next(nullptr) { }
     ReviewNode(string c, double r, ReviewNode* next): comment(c), rating(r), next(next) { }
- }
+ };
 
- int main() {
- }
+void push_front(ReviewNode* next, double rating, string comment);
 
+void push_back(ReviewNode* next, double rating, string comment);
+
+void outputReviews(ReviewNode* next);
+
+int main() {
+    ReviewNode* head;
+
+    cout << "Which linked list method should we use?" << endl
+         << "\t[1] New nodes are added at the head of the linked list" << endl
+         << "\t[2] New nodes are added at the tail of the linked list" << endl
+         << "\tChoice: > ";
+    
+}
+
+void push_front(ReviewNode* next, double rating, string comment) {
+
+}
+
+void push_back(ReviewNode* next, double rating, string comment) {
+
+}
+
+void outputReviews(ReviewNode* next) {
+
+}
