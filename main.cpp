60f97dfc0d9d1014683aea47d34f8dcdf64589cd
rylander925 <rylander925@gmail.com>
2025-10-01 22:14:55 -0700

    Filled functions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ IDE Used: Visual Studio Code
  */
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 
@@ -19,11 +20,11 @@ IDE Used: Visual Studio Code
     ReviewNode(string c, double r, ReviewNode* next): comment(c), rating(r), next(next) { }
  };
 
-void push_front(ReviewNode* next, double rating, string comment);
+void push_front(ReviewNode* head, double rating, string comment);
 
-void push_back(ReviewNode* next, double rating, string comment);
+void push_back(ReviewNode* head, double rating, string comment);
 
-void outputReviews(ReviewNode* next);
+void outputReviews(ReviewNode* head);
 
 int main() {
     ReviewNode* head;
@@ -35,14 +36,35 @@ int main() {
     
 }
 
-void push_front(ReviewNode* next, double rating, string comment) {
-
+void push_front(ReviewNode* head, double rating, string comment) {
+    ReviewNode* newNode = new ReviewNode(comment, rating); //initialized w/ nullptr by default
+    if (head) {
+        newNode->next = head;
+    }
+    head = newNode;
 }
 
-void push_back(ReviewNode* next, double rating, string comment) {
-
+void push_back(ReviewNode* head, double rating, string comment) {
+    ReviewNode* current = head;
+    ReviewNode* newNode = new ReviewNode(comment, rating); //initialized w/ nullptr by default
+    if (!head) { //head is the new node if empty
+        head = newNode;
+    } else { //otherwise traverse & add to end
+        while (current->next) { current = current->next; }
+        current->next = newNode;
+    }
 }
 
-void outputReviews(ReviewNode* next) {
-
+void outputReviews(ReviewNode* head) {
+    ReviewNode *current = head;
+    int reviewNumber = 1;
+    double sum = 0;
+    cout << fixed << setprecision(2);
+    cout << "Outputting all reviews:" << endl;
+    while (current) {
+        cout << "\t> Review #" << reviewNumber << ": " << current->rating << ": " << current->comment << endl;
+        sum +- current->rating;
+        reviewNumber++;
+    }
+    cout << "\t> Average: " << sum / reviewNumber << endl;
 }
